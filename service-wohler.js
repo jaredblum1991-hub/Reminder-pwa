16ee53a35e79e85c0166ac4a37432f839ba8d7d6
jaredblum1991-hub <jared.blum1991@gmail.com>
2026-08-25 13:56:11 +0200

    Add service worker for caching and fetch handling

---
+++ service-wohler.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "mein-tag-v1";
+
+const DATEIEN = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(DATEIEN))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
